8ee6b2cab5085cd1d8dfdd10431353eee868a53d
Sandeep_Kumar_Jha <sjha30053@gmail.com>
2026-04-26 16:10:41 +0530

    fix frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Header from '../components/Header'
-import Steps from '../components/steps'
-import Bgslider from '../components/Bgslider'
+import Steps from '../components/Steps'
+import BgSlider from '../components/BgSlider'
 import Testimonials from '../components/Testimonials'
 import Upload from '../components/Upload'
 
 const Home = () => {
   return (
     <div>
-    <Header />
-    <Steps />
-    <Bgslider />
-    <Testimonials />
-    <Upload />
+      <Header />
+      <Steps />
+      <BgSlider />
+      <Testimonials />
+      <Upload />
     </div>
   )
 }
